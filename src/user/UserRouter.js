55379c681aaaed9dfd21aea94ba277c02b1bca8d
Roberto Sousa <robertosousa186@gmail.com>
2021-03-08 20:56:34 -0300

    creating first validation test

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const UserService = require('./UserService');
 
 router.post('/api/1.0/users', async (req, res) => {
+  const user = req.body;
+  if (user.username === null) {
+   return res.status(400).send();
+  }
+
   await UserService.save(req.body);
   return res.send({ message: 'User created.' });
 });
